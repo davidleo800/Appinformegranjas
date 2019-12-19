bde7f5c6835ae71e5e953eaecbd610fa7d726564
David <daviud@live.com>
2019-12-19 00:45:49 -0500

    Agregar archivos de proyecto.

---
+++ App1/App1/View/ViewMortalidad.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppInformeGranjas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ViewMortalidad : ContentPage
+	{
+		public ViewMortalidad ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
