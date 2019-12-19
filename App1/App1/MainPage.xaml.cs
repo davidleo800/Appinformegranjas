84a7df27b2e5b107fd55327fd1924a29e329b883
David <daviud@live.com>
2019-12-19 02:32:17 -0500

    Cambio 19-12-19

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace App1
+﻿namespace App1
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+    using AppInformeGranjas;
+    using AppInformeGranjas.View;
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            //Boton que redirige a vista formulario ViewMortalidad
+            btnMort.Clicked += (sender, e) => {
+                ((NavigationPage)this.Parent).PushAsync(new ViewMortalidad());
+            };
+            //Boton que redirige a vista formulario ViewBultosConsu
+            btnBultos.Clicked += (sender, e) => {
+                ((NavigationPage)this.Parent).PushAsync(new ViewBultosConsu());
+            };
+            //Boton que redirige a vista formulario ViewPesoPromedio
+            btnPeso.Clicked += (sender, e) => {
+                ((NavigationPage)this.Parent).PushAsync(new ViewPesoPromedio());
+            };
         }
     }
-}
+    }
+
