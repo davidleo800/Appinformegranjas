a6d13d8fac8966f7c134f6a480239a1ba4db7e59
Nicolas Novoa Martinez <nicolas.novoam@unilibrebog.edu.co>
2020-01-20 15:00:35 -0500

    cambios de esta mañana 20 de enero.

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
+using AppInformeGranjas.DataBase;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
 namespace App1
 {
     public partial class App : Application
     {
+        static DataBaseMort database;
+
+        public static DataBaseMort Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new DataBaseMort(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
--- App1/App1/View/ViewMortalidad.xaml.cs
+++ App1/App1/View/ViewMortalidad.xaml.cs
@@ -2,35 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using AppInformeGranjas.Models;
+
 namespace AppInformeGranjas
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ViewMortalidad : ContentPage
 	{
-		public ViewMortalidad ()
+		public ViewMortalidad()
 		{
-			InitializeComponent ();
+			InitializeComponent();
 			listaRegistros();
+
+			btn1.Clicked += (sender, e) => {
+				((NavigationPage)this.Parent).PushAsync(new DataBase.DataPage());
+			};
 		}
+		
 
 		private async void listaRegistros() {
 
-			try {
-				ManagerMort manager = new ManagerMort();
-				var res = await manager.getMoritalidad();
+		try {
+		ManagerMort manager = new ManagerMort();
+		var res = await manager.getMoritalidad();
 
 				if (res != null) {
-					lstMort.ItemsSource = res;
-				}
-			}
-			catch (Exception e1) {
-			
-			}
+		lstMort.ItemsSource = res;
+		}
+		}
+				catch (Exception e1) {
+
+		}
 		}
 
 	}
